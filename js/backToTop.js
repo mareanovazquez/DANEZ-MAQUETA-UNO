c90496087f93ec649eec6791b3670b5664f3e1ea
Mariano Vázquez <mareanovazquez@protonmail.com>
2025-02-27 12:41:52 -0300

    mejoras en js back to top

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Mostrar y ocultar botón "Volver arriba"
-    var btnBackToTop = document.getElementById('btnBackToTop');
+    let btnBackToTop = document.getElementById('btnBackToTop');
 
     window.addEventListener('scroll', function() {
         // Puedes cambiar 'nosotros' por la sección a partir de la cual
         // quieres que aparezca el botón, ya que 'experiencias' no existe en tu HTML
-        var seccionReferencia = document.getElementById('nosotros');
-        var seccionOffsetTop = seccionReferencia.getBoundingClientRect().top + window.scrollY;
+        let seccionReferencia = document.getElementById('nosotros');
+        let seccionOffsetTop = seccionReferencia.getBoundingClientRect().top + window.scrollY;
 
         if (window.scrollY >= seccionOffsetTop) {
             btnBackToTop.style.marginLeft = '0px';
